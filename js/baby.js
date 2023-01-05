b5abf39a67b7c088c2b65d7e5a1551863582234c
mywaybin <118698699+mywaybin@users.noreply.github.com>
2023-01-05 22:34:15 +0900

    20230105

--- js/baby.js
+++ js/baby.js
@@ -39,7 +39,42 @@ $(function () {
 
     $('.mainContent .arrows .Next').on('click', function () {
         $('.main_pro_slide').slick('slickNext')
+    })
+    // mainSlide
+    $('.mainSlide02').slick({
+        //slidesToShow: 2,
+        //arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        pauseOnFocus: false
     });
+
+    $('.mainSlide02').on('afterChange', function (e, s, c) {
+        $('.main_dots li').eq(c).addClass('on')
+            .siblings().removeClass('on');
+        $('.mainVisual .main_num').text(c + 1)
+    });
+
+    $('.main_dots li').on('click', function () {
+        var idx = $(this).index();
+        $('.mainSlide02').slick('slickGoTo', idx)
+    })
+
+    $('.mainSlide02').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 3,
+    });
+
+    $('.mainBanner .arrows .Prev').on('click', function () {
+        $('.mainSlide02').slick('slickPrev')
+    });
+
+    $('.mainBanner .arrows .Next').on('click', function () {
+        $('.mainSlide02').slick('slickNext')
+    })
 
 })
 
